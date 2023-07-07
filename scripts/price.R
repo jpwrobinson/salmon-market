74ca5daa981640d49da11518183b58f3552adb6b
James Robinson <jpwrobinson@gmail.com>
2023-07-07 15:45:24 +0100

    ons plot

---
+++ scripts/price.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(cowplot)
+library(magrittr)
+
+# dat
+chi<-read.csv('data/ons/fresh_chicken_per_kgseries-070723.csv', skip=7) %>% 
+      set_colnames(c("year", 'gbp_kg')) %>% 
+    filter(!str_length(year) > 4) %>% 
+    mutate(food = 'chicken')
+beef<-read.csv('data/ons/beefmince_kg_series-070723.csv', skip=7) %>% 
+      set_colnames(c("year", 'gbp_kg')) %>% 
+    filter(!str_length(year) > 4) %>%
+    mutate(food = 'beef')
+salm<-read.csv('data/ons/salmon_fillets_kg_series-070723.csv', skip=7) %>% 
+      set_colnames(c("year", 'gbp_kg')) %>% 
+    filter(!str_length(year) > 4) %>% 
+    filter(year > 2001) %>% 
+    mutate(food = 'salmon')
+white<-read.csv('data/ons/whitefish_fillets_series-070723.csv', skip=7) %>% 
+    set_colnames(c("year", 'gbp_kg')) %>% 
+    filter(!str_length(year) > 4) %>% 
+    mutate(food = 'whitefish')
+
+names(white)
+price<-rbind(chi, beef, salm, white) %>% 
+    mutate(year = as.Date(year, '%Y'))
+
+ggplot(price, aes(year, gbp_kg, col=food)) + 
+    geom_line(aes(group=food)) +
+    labs(x = '', y = '£ per kg') +
+    scale_x_date()
